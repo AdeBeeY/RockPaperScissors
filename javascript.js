a814b1fdda92f6caee8340a0ed797ade349db3ff
AdeBeeY <adewusiadebayo@gmail.com>
2023-08-30 03:06:09 +0100

    rename .js file

--- javascript.js
+++
@@ -1,5 +0,0 @@
-const option = ["Rock", "Scissors", "Paper"];
-function getComputerChoice() {
-    let computerChoice = option[Math.floor(Math.random() * option.length)];
-    return computerChoice;
-}
