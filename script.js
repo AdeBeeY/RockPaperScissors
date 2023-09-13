15b26980b65abbaaec1fbe5c3d90a228808e486d
AdeBeeY <adewusiadebayo@gmail.com>
2023-09-13 14:09:32 +0100

    create game function that plays five times

--- script.js
+++ script.js
@@ -4,24 +4,37 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    if(computerSelection === "rock" && playerSelection === "rock" || computerSelection == "paper" && playerSelection == "paper" || computerSelection == "scissors" && playerSelection == "scissors") {
-        return "Draw! Play Again";
-    } else if(computerSelection === "rock" && playerSelection === "scissors") {
-        return "Computer Wins! Rock beats Scissors";
-    } else if(computerSelection === "scissors" && playerSelection === "rock") {
-        return "You Won! Rock beats Scissors";
-    } else if(computerSelection === "scissors" && playerSelection === "paper") {
-        return "Computer Wins! Scissors beats Paper";
-    } else if(computerSelection === "paper" && playerSelection === "scissors") {
-        return "You Won! Scissors beats Paper";
-    } else if(computerSelection === "paper" && playerSelection === "rock") {
-        return "Computer Wins! Paper beats Rock";
-    } else if(computerSelection === "rock" && playerSelection === "paper") {
-        return "You Won! Paper beats Rock";
+function game(){
+    function playRound(playerSelection, computerSelection) {
+        if(computerSelection === "rock" && playerSelection === "rock" || computerSelection == "paper" && playerSelection == "paper" || computerSelection == "scissors" && playerSelection == "scissors") {
+            return `Draw! Player chose ${playerSelection} Computer chose ${computerSelection}`;
+        } else if(computerSelection === "rock" && playerSelection === "scissors") {
+            return `Computer Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
+        } else if(computerSelection === "scissors" && playerSelection === "rock") {
+            return `Player Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
+        } else if(computerSelection === "scissors" && playerSelection === "paper") {
+            return `Computer Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
+        } else if(computerSelection === "paper" && playerSelection === "scissors") {
+            return `Player Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
+        } else if(computerSelection === "paper" && playerSelection === "rock") {
+            return `Computer Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
+        } else if(computerSelection === "rock" && playerSelection === "paper") {
+            return `Player Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
+        }
+        playerSelection = prompt("choose", "").toLowerCase();
+        computerSelection = getComputerChoice();
+        let result = console.log(playRound(playerSelection, computerSelection));
+        return result;
     }
+
+
+        let playerSelection;
+        let computerSelection;
+        //playRound(playerSelection, computerSelection);
+
+        for (let i = 1; i <= 5; i++) {
+            playRound(playerSelection, computerSelection);
+        }
 }
 
-const playerSelection = "ROCK";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+game();
