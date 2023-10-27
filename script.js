517e9af39d6d4662b0bbf0a05a3e7942edf2e468
AdeBeeY <adewusiadebayo@gmail.com>
2023-10-27 11:34:42 +0100

    Revamped the functions To get adequate results for Score and Final Result

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
+
 const option = ["rock", "scissors", "paper"];
 function getComputerChoice() {
-    let computerChoice = option[Math.floor(Math.random() * option.length)];
-    return computerChoice;
+  let computerChoice = option[Math.floor(Math.random() * option.length)];
+  return computerChoice;
 }
 
-function game(){
-    function playRound(playerSelection, computerSelection) {
-        if(computerSelection === "rock" && playerSelection === "rock" || computerSelection == "paper" && playerSelection == "paper" || computerSelection == "scissors" && playerSelection == "scissors") {
-            return `Draw! Player chose ${playerSelection} Computer chose ${computerSelection}`;
-        } else if(computerSelection === "rock" && playerSelection === "scissors") {
-            return `Computer Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
-        } else if(computerSelection === "scissors" && playerSelection === "rock") {
-            return `Player Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
-        } else if(computerSelection === "scissors" && playerSelection === "paper") {
-            return `Computer Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
-        } else if(computerSelection === "paper" && playerSelection === "scissors") {
-            return `Player Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
-        } else if(computerSelection === "paper" && playerSelection === "rock") {
-            return `Computer Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
-        } else if(computerSelection === "rock" && playerSelection === "paper") {
-            return `Player Wins! Player chose ${playerSelection} Computer chose ${computerSelection}`;
-        }
-        playerSelection = prompt("choose", "").toLowerCase();
-        computerSelection = getComputerChoice();
-        let result = console.log(playRound(playerSelection, computerSelection));
-        return result;
-    }
+function playRound(playerSelection, computerSelection) {
+  if(computerSelection === "rock" && playerSelection === "rock" || computerSelection === "paper" && playerSelection === "paper" || computerSelection === "scissors" && playerSelection === "scissors") {
+    return 'Tie';
+  } else if(computerSelection === "rock" && playerSelection === "scissors" || computerSelection === "scissors" && playerSelection === "paper" || computerSelection === "paper" && playerSelection === "rock") {
+    return 'You Lose!';
+  } else if(computerSelection === "scissors" && playerSelection === "rock" || computerSelection === "paper" && playerSelection === "scissors" || computerSelection === "rock" && playerSelection === "paper") {
+    return 'You Won!';
+  };
+};
 
+function game() {
+  let playerSelection;
+  let computerSelection;
+  let playerScore = 0;
+  let computerScore = 0;
+  let draw = 0;
 
-        let playerSelection;
-        let computerSelection;
-        //playRound(playerSelection, computerSelection);
+  for (let round = 1; round <= 5; round++) {
+    playerSelection = prompt("Choose", "").toLowerCase();
+    computerSelection = getComputerChoice();
 
-        for (let i = 1; i <= 5; i++) {
-            playRound(playerSelection, computerSelection);
-        }
-}
+    let result = playRound(playerSelection, computerSelection);
+
+    if (result === 'Tie') {
+      draw++;
+    } else if (result === 'You Lose!') {
+      computerScore++;
+    } else if (result === 'You Won!') {
+      playerScore++;
+    };
+    console.log(`Result: ${result} \n Player Chose: ${playerSelection} and Computer Chose: ${computerSelection} \n Score: \n Draw: ${draw}, Player Score: ${playerScore}, Computer Score: ${computerScore}`);
+  };
+
+  if (playerScore > computerScore) {
+    console.log(`Final Result: Player Wins with total score of: ${playerScore}`);
+  } else if (playerScore < computerScore) {
+    console.log(`Final Result: Computer Wins with total score of: ${computerScore}`);
+  } else if (playerScore === computerScore) {
+    console.log(`Draw! There is no winner as Player Score is: ${playerScore} and Computer Score i ${computerScore}`);
+  };
+};
 
 game();
