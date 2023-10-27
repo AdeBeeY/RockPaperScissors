acd11c4c1f8d6700f621275334b06d6883e7e974
AdeBeeY <adewusiadebayo@gmail.com>
2023-10-28 00:14:11 +0100

    Modify Functions
    To accommodate user interface

--- script.js
+++ script.js
@@ -1,9 +1,85 @@
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let draw = 0;
+
+const rockBtnElement = document.querySelector('.js-rock-button');
+rockBtnElement.addEventListener('click', () => {
+  computerSelection = getComputerChoice();
+  playerSelection = 'rock';
+  let result = playRound(playerSelection, computerSelection);
+
+  if (result === `Draw! ${playerSelection} equals ${computerSelection}`) {
+    draw++;
+  } else if (result === `You Lose! ${computerSelection} beats ${playerSelection}`) {
+    computerScore++;
+  } else if (result ===`You Win! ${playerSelection} beats ${computerSelection}`) {
+    playerScore++;
+  }
+  divElement.textContent = `Result: ${result}, Score: PlayerScore: ${playerScore}, Computer Score: ${computerScore}, Draw: ${draw}`;
+
+  if (playerScore === 5) {
+    winnerElement.textContent = `Player is the Winner with the Total Score of ${playerScore}`;
+  } else if (computerScore === 5) {
+    winnerElement.textContent = `Computer is the Winner with the Total Score of ${computerScore}`;
+  };
+});
+
+const paperBtnElement = document.querySelector('.js-paper-button');
+paperBtnElement.addEventListener('click', () => {
+  playerSelection = 'paper';
+  computerSelection = getComputerChoice();
+  let result = playRound(playerSelection, computerSelection);
+
+  if (result === `Draw! ${playerSelection} equals ${computerSelection}`) {
+    draw++;
+  } else if (result === `You Lose! ${computerSelection} beats ${playerSelection}`) {
+    computerScore++;
+  } else if (result ===`You Win! ${playerSelection} beats ${computerSelection}`) {
+    playerScore++;
+  }
+  divElement.textContent = `Result: ${result}, Score: PlayerScore: ${playerScore}, Computer Score: ${computerScore}, Draw: ${draw}`;
+
+  if (playerScore === 5) {
+    winnerElement.textContent = `Player is the Winner with the Total Score of ${playerScore}`;
+  } else if (computerScore === 5) {
+    winnerElement.textContent = `Computer is the Winner with the Total Score of ${computerScore}`;
+  };
+});
+
+const scissorsBtnElement = document.querySelector('.js-scissors-button');
+scissorsBtnElement.addEventListener('click', () => {
+  playerSelection = 'scissors';
+  computerSelection = getComputerChoice();
+  let result = playRound(playerSelection, computerSelection);
+
+  if (result === `Draw! ${playerSelection} equals ${computerSelection}`) {
+    draw++;
+  } else if (result === `You Lose! ${computerSelection} beats ${playerSelection}`) {
+    computerScore++;
+  } else if (result ===`You Win! ${playerSelection} beats ${computerSelection}`) {
+    playerScore++;
+  }
+  divElement.textContent = `Result: ${result}, Score: PlayerScore: ${playerScore}, Computer Score: ${computerScore}, Draw: ${draw}`;
+
+  if (playerScore === 5) {
+    winnerElement.textContent = `Player is the Winner with the Total Score of ${playerScore}`;
+  } else if (computerScore === 5) {
+    winnerElement.textContent = `Computer is the Winner with the Total Score of ${computerScore}`;
+  };
+});
+
+const divElement = document.querySelector('.js-result-div');
+const winnerElement = document.querySelector('.js-winner-div');
 
 const option = ["rock", "scissors", "paper"];
 function getComputerChoice() {
   let computerChoice = option[Math.floor(Math.random() * option.length)];
   return computerChoice;
-}
+};
+
+computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
   if(computerSelection === "rock" && playerSelection === "rock" || computerSelection === "paper" && playerSelection === "paper" || computerSelection === "scissors" && playerSelection === "scissors") {
@@ -14,37 +90,3 @@ function playRound(playerSelection, computerSelection) {
     return `You Win! ${playerSelection} beats ${computerSelection}`;
   };
 };
-
-function game() {
-  let playerSelection;
-  let computerSelection;
-  let playerScore = 0;
-  let computerScore = 0;
-  let draw = 0;
-
-  for (let round = 1; round <= 5; round++) {
-    playerSelection = prompt("Choose", "").toLowerCase();
-    computerSelection = getComputerChoice();
-
-    let result = playRound(playerSelection, computerSelection);
-
-    if (result === `Draw! ${playerSelection} equals ${computerSelection}`) {
-      draw++;
-    } else if (result === `You Lose! ${computerSelection} beats ${playerSelection}`) {
-      computerScore++;
-    } else if (result === `You Win! ${playerSelection} beats ${computerSelection}`) {
-      playerScore++;
-    };
-    console.log(`Result: ${result} \n Player Chose: ${playerSelection} and Computer Chose: ${computerSelection} \n Score: \n Draw: ${draw}, Player Score: ${playerScore}, Computer Score: ${computerScore}`);
-  };
-
-  if (playerScore > computerScore) {
-    console.log(`Final Result: Player Wins with total score of: ${playerScore}`);
-  } else if (playerScore < computerScore) {
-    console.log(`Final Result: Computer Wins with total score of: ${computerScore}`);
-  } else if (playerScore === computerScore) {
-    console.log(`Draw! There is no winner as Player Score is: ${playerScore} and Computer Score i ${computerScore}`);
-  };
-};
-
-game();
